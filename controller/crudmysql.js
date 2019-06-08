2a56de9ef7eb92d5ccf628b9f6d3d6790b78f3a2
shubham mehta <shubahm.mehta@gmail.com>
2019-06-08 22:53:56 +0530

    GET UPDATE DELETE API

--- controller/crudmysql.js
+++ controller/crudmysql.js
@@ -5,15 +5,6 @@ exports.createInfo = (req, res, next) => {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const city = req.body.city;
-    //  const user = new User(firstName, lastName, city);
-    /*  db.execute('INSERT INTO `userinfo2` (`firstName`,`lastName`,`city`) VALUES ('firstName','lastName','')')
-          .then(result => {
-              res.status(201).json(JSON.stringify(result[0]));
-              //  console.log(result[0], result[1]);
-          })
-          .catch(err => {
-              console.log(err);
-          }); */
     User.create({
         firstName: firstName,
         lastName: lastName,
@@ -27,11 +18,31 @@ exports.createInfo = (req, res, next) => {
 
 }
 exports.updateInfo = (req, res, next) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const city = req.body.city;
+
+    User.update({ firstName: firstName, lastName: lastName, city: city }, { where: { id: req.params.id } }).then((result => {
+        res.status(200).json(result);
+    })).catch(err => res.status(500).json(err))
 
 }
 exports.deleteInfo = (req, res, next) => {
+    User.destroy({ where: { id: req.params.id } }).then((result => {
+        res.status(200).json(result);
+
+    })).catch(err => {
+        res.status(500).json(err)
+    });
 
 }
 exports.getinfo = (req, res, next) => {
+    User.findAll({}).then(users => {
+        console.log(JSON.stringify(users));
+        res.status(200).json(users);
+
+    }).catch(err =>
+        res.status(500).json(err)
+    );
 
 }
